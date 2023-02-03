df79b814679c1cd0631e8ceb90426776950d8607
Facundo Ojeda <facuojeda1@outlook.com>
2023-02-02 23:53:18 -0300

    Proyecto Terminado

--- js/function.js
+++ js/function.js
@@ -1,10 +1,12 @@
 //
+
 function CopyEmail(){
     let text=document.getElementById('TextEmail');
     let button=document.getElementById('CopyEmail');
 
     navigator.clipboard.writeText(text.textContent);
-    document.getElementById("CopyEmail").style.color="rgb(253, 159, 83)"
+    document.getElementById("CopyEmail").style.color="black"
+    document.getElementById("CopyEmail").style.fontSize="12px"
     document.getElementById("CopyEmail").innerHTML="¡Copiado!";
     setTimeout(() => {
         document.getElementById("CopyEmail").innerHTML="<i class='fa fa-clone'></i>";  
@@ -16,13 +18,15 @@ function CopyPhone(){
     let button=document.getElementById('CopyPhone');
 
     navigator.clipboard.writeText(text.textContent);
-    document.getElementById("CopyPhone").style.color="rgb(253, 159, 83)"
+    document.getElementById("CopyPhone").style.color="black"
+    document.getElementById("CopyPhone").style.fontSize="12px"
     document.getElementById("CopyPhone").innerHTML="¡Copiado!";
     setTimeout(() => {
         document.getElementById("CopyPhone").innerHTML="<i class='fa fa-clone'></i>";  
     },[1500] )
 
 }
+
 //
 const datos = `{
     "nombre": "GUILLERMO RODRIGUEZ",
@@ -38,6 +42,7 @@ document.getElementById("nacionalidad").innerHTML = obj.nacionalidad;
 document.getElementById("TextEmail").innerHTML = obj.email;
 document.getElementById("nombre").innerHTML = obj.nombre;
 document.getElementById("TextPhone").innerHTML = obj.cel;
+
 //
 
 let esconderBTN = document.getElementById('HideBTN');
@@ -56,3 +61,20 @@ function ShowText() {
 }
 
 //
+
+const audio = new Audio();
+audio.src = "https://www.fesliyanstudios.com/play-mp3/387";
+
+//
+
+function clickCounter() {
+    if (typeof(Storage) !== "undefined") {
+      if (localStorage.clickcount) {
+        localStorage.clickcount = Number(localStorage.clickcount)+1;
+      } else {
+        localStorage.clickcount = 1;
+      }
+    }
+  }
+  
+  //
